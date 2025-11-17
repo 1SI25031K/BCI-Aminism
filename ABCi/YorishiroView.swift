0abffa1e0df96c5d6a6fe72c67fbdfe3336be83d
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-11-17 22:42:49 +0900

    Implement global black and white theme using Color Assets

---
+++ ABCi/YorishiroView.swift
@@ -0,0 +1,18 @@
+//
+//  YorishiroView.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/17.
+//
+
+import SwiftUI
+
+struct YorishiroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YorishiroView()
+}
