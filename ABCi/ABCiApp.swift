3921af44c5eb418d1d686226b3c439020dfbdd1f
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-11-17 21:42:45 +0900

    Initial Commit

---
+++ ABCi/ABCiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCiApp.swift
+//  ABCi
+//
+//  Created by Kosei Miyamoto on 2025/11/17.
+//
+
+import SwiftUI
+
+@main
+struct ABCiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABCiTests/ABCiTests.swift
@@ -0,0 +1,16 @@
+//
+//  ABCiTests.swift
+//  ABCiTests
+//
+//  Created by Kosei Miyamoto on 2025/11/17.
+//
+
+import Testing
+
+struct ABCiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
